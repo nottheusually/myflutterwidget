cb65418bfb4fb5b81d54cea7d91b07a9f0c8a646
Ameh Sunday Itodo <sundayitodojnr@gmail.com>
2024-01-03 09:47:08 -0500

    android manifest to enable internet connection on app when releaseds

---
+++ andriodmanifest.dart
@@ -0,0 +1,9 @@
+//to be added inside android manifest.xml inside andriod folder,
+//res,
+//andriodManifest.xml
+<uses-permission android:name="android.permission.USE_FINGERPRINT"/>
+    <uses-permission android:name="android.permission.INTERNET"/>
+    <uses-permission android:name="android.permission.USE_BIOMETRIC"/>
+
+    //it enables the app to login to the internet when it's finally on 
+    //release mode
